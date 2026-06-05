16c88c8bfc15198ad42bf58f53c9748fe5a850fa
Andrija Siskovic <andrija.siskovic@gmail.com>
2026-06-05 11:57:04 +0200

    Fix reader modal mobile layering

--- public/pwa.js
+++ public/pwa.js
@@ -9,7 +9,7 @@
   });
 
   window.addEventListener("load", function onLoad() {
-    navigator.serviceWorker.register("./sw.js?v=20260605-reader").catch(function ignoreRegistrationError() {
+    navigator.serviceWorker.register("./sw.js?v=20260605-reader-layer").catch(function ignoreRegistrationError() {
       // The app still works as a normal website if service workers are unavailable.
     });
   });
